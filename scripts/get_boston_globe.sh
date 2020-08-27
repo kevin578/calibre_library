8bf8f5f306bf17f55647fa7e6841439cc632c5ab
root <kevinbriggs1@gmail.com>
2020-08-27 12:22:45 +0000

    script updates

--- scripts/get_boston_globe.sh
+++ scripts/get_boston_globe.sh
@@ -1,2 +1,2 @@
-docker exec -it calibre ebook-convert "Boston Globe Subscription".recipe /config/boston_globe.epub
+docker exec -it calibre ebook-convert "Boston Globe Subscription".recipe /config/boston_globe.epub 
 docker exec -it calibre calibredb add  --with-library http://localhost:8081/#calibre_library /config/boston_globe.epub
--- scripts/get_new_yorker.sh
+++ scripts/get_new_yorker.sh
@@ -1,2 +1,2 @@
-docker exec -it calibre ebook-convert "New Yorker Magazine".recipe new_yorker.epub
+docker exec -it calibre ebook-convert "New Yorker Magazine".recipe /config/new_yorker.epub
 docker exec -it calibre calibredb add  --with-library http://localhost:8081/#calibre_library /config/new_yorker.epub
